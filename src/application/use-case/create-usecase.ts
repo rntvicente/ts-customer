3a145547739d17d691d9162a5da8c6878c0afa1e
Renato Vicente <rntvicente@gmail.com>
2023-09-29 23:25:53 -0300

    feat: criação de usecase

---
+++ src/application/use-case/create-usecase.ts
@@ -0,0 +1,26 @@
+import CustomerRepository from '../repository/customer-repository';
+import { Logger } from '../../config/logger/logger';
+import { CustomerType, Customer } from '../../domain/customer-entity';
+
+import { Usecase } from './use-case.interface';
+import { String } from 'aws-sdk/clients/apigateway';
+
+export class Create implements Usecase {
+  constructor(
+    private readonly repository: CustomerRepository,
+    private readonly logger: Logger
+  ) {}
+
+  async execute(input: input): Promise<String> {
+    this.logger.info(`[USE CASE] starting create customer: ${input}`);
+
+    const customer = await Customer.create(input);
+    const id = await this.repository.add(customer);
+
+    this.logger.info(`[USE CASE] customer add successfully ID ${id}`);
+
+    return id;
+  }
+}
+
+export type input = Omit<CustomerType, 'id'>;
