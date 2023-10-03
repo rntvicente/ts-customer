61e84fae6c1ee8a601a96ea72f84834ba0aef081
Renato Vicente <rntvicente@gmail.com>
2023-10-02 23:32:07 -0300

    feat: criação

---
+++ src/application/search/search-customer-controller.ts
@@ -0,0 +1,24 @@
+import { Request } from 'express';
+
+import { Controller } from '../../infra/controller/handle';
+import { HttpResponse } from '../../infra/server/server';
+import { Logger } from '../../config/logger/logger';
+import { Usecase } from '../../config/use-case';
+
+export class SearchCustomerController implements Controller {
+  constructor(
+    private readonly usecase: Usecase,
+    private readonly logger: Logger
+  ) {}
+
+  async handle({ query }: Request): Promise<HttpResponse> {
+    this.logger.info(`[CONTROLLER] init search customer by ${query}`);
+
+    const customers = await this.usecase.execute(query);
+
+    return {
+      statusCode: 200,
+      body: customers,
+    };
+  }
+}
